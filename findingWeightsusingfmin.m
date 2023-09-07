4b6edad8fb8f667f3f5261b3bd06eb3c38fd6974
MasterNomi <33470736+MasterNomi@users.noreply.github.com>
2023-09-07 23:19:39 +0500

    Update findingWeightsusingfmin.m

--- findingWeightsusingfmin.m
+++ findingWeightsusingfmin.m
@@ -28,3 +28,36 @@ optimal_w2 = optimal_w(2);
 fprintf('Optimal w1: %f\n', optimal_w1);
 fprintf('Optimal w2: %f\n', optimal_w2);
 fprintf('Optimal MSE: %f\n', mse_optimal);
+
+%%
+% Generate synthetic data
+rng(0); % Set random seed for reproducibility
+u1 = rand(100, 1);
+u2 = rand(100, 1);
+u3 = rand(100, 1);
+noise = 0.1 * randn(100, 1);
+y_true = 2.0 * u1 .* u2 .* (0.5 * exp(-0.2 * u3)); % True values of coefficients
+
+% Create a function that represents the model equation
+model = @(coefficients, X) coefficients(1) * X(:, 1) .* X(:, 2) .* (coefficients(2) * exp(coefficients(3) * X(:, 3)));
+
+% Define an initial guess for the coefficients
+initial_guess = [1, 1, 1];
+
+% Define a loss function (e.g., mean squared error)
+loss_function = @(coefficients) mean((y_true - model(coefficients, [u1, u2, u3])).^2);
+
+% Optimize the coefficients using a numerical optimization method
+options = optimset('Display', 'iter'); % Adjust optimization options as needed
+estimated_coefficients = fminsearch(loss_function, initial_guess, options);
+
+% Extract the estimated coefficients
+w1 = estimated_coefficients(1);
+v1 = estimated_coefficients(2);
+v2 = estimated_coefficients(3);
+
+% Display the estimated coefficients
+disp(['Estimated w1: ', num2str(w1)]);
+disp(['Estimated v1: ', num2str(v1)]);
+disp(['Estimated v2: ', num2str(v2)]);
+
